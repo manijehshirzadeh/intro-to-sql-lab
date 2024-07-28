db09b3530daa9bac55d1f41bbbbdff6f82ca3658
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-28 12:12:55 +1000

    solved clue 6

--- clues.sql
+++ clues.sql
@@ -44,6 +44,7 @@ SELECT name  FROM cities WHERE name LIKE 'Serra%' LIMIT 1;
 -- follow right behind you!
 
 -- Write SQL query here
+SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE code = ( SELECT countrycode  FROM cities WHERE name LIKE 'Serra%' LIMIT 1));
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
